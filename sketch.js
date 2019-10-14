ba881cb15daab96cf76a9da080eb3d7543c4e889
Martina <martinamelillo97@gmail.com>
2019-10-14 18:06:44 +0200

    assets

--- sketch.js
+++ sketch.js
@@ -1,11 +1,35 @@
+var mySong;
+var analyzer;
+
 function preload(){
-  // put preload code here
+  mySong = loadSound("./assets/8BIT.mp3");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(20);
+
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(mySong);
 }
 
 function draw() {
-  // put drawing code here
+  var volume = 0;
+  if (mouseIsPressed){
+    if(mySong.isPlaying() == false){
+      mySong.play();
+    }
+
+  var startLines = 500; //dove iniziano le linee
+  var y = height/2;
+  var hLines = 5; //altezza linee
+
+  volume = analyzer.getLevel();
+  volume = map(volume, 0, 1, 0, height);
+
+  for(var x = startLines; x < windowWidth - startLines; x+= 10){
+    stroke(255);
+    line(x, y - hLines, x, y + volume);
+    }
+  }
 }
