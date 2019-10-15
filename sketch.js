5e96d607be0ec4aad223ba3cb2d2152058804d3b
Martina <martinamelillo97@gmail.com>
2019-10-15 17:36:16 +0200

    Sketch

--- sketch.js
+++ sketch.js
@@ -1,35 +1,84 @@
+var myImage;
+
 var mySong;
 var analyzer;
 
-function preload(){
-  mySong = loadSound("./assets/8BIT.mp3");
+var allLines = [];
+var startLines = 500;
+var h = 5;
+
+function preload() {
+  myImage = loadImage("./assets/gengar.gif")
+  mySong = loadSound("./assets/zombie.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(20);
+  background(0);
 
   analyzer = new p5.Amplitude();
   analyzer.setInput(mySong);
+
+  for (var x = startLines; x < windowWidth - startLines; x += 10) {
+    var tempx = x;
+    var tempy1 = height / 2;
+    var tempy2 = height / 2 + h;
+
+    var tempLine = new Line(tempx, tempy1, tempx, tempy2);
+
+    allLines.push(tempLine);
+  }
 }
 
 function draw() {
-  var volume = 0;
-  if (mouseIsPressed){
-    if(mySong.isPlaying() == false){
+
+  background(0);
+
+  imageMode(CENTER);
+  image(myImage, width / 2, height / 2 + 200, myImage.width / 4, myImage.height / 4);
+
+  if (mouseIsPressed) {
+    if (mySong.isPlaying() == false) {
       mySong.play();
+    } else {
+      mySong.stop();
     }
+  }
 
-  var startLines = 500; //dove iniziano le linee
-  var y = height/2;
-  var hLines = 5; //altezza linee
-
+  var volume = 0;
   volume = analyzer.getLevel();
-  volume = map(volume, 0, 1, 0, height);
+  volume = map(volume, 0, 1, height / 2, -5);
 
-  for(var x = startLines; x < windowWidth - startLines; x+= 10){
-    stroke(255);
-    line(x, y - hLines, x, y + volume);
-    }
+  for (var i = 0; i < allLines.length; i++) {
+    var tempLine = allLines[i];
+
+    tempLine.y2 = volume;
+
+    tempLine.display();
+  }
+
+  var myText = "Are you afraid of ghosts?";
+
+  textFont('VT323');
+  drawingContext.font = "34px VT323";
+  drawingContext.textAlign = "left";
+  fill(255);
+  text(myText, startLines, height / 2 + 30);
+
+}
+
+function Line(_x1, _y1, _x2, _y2) {
+  this.x1 = _x1;
+  this.y1 = _y1;
+  this.x2 = _x2;
+  this.y2 = _y2;
+  this.color = stroke(255, 30, 121);
+
+  this.display = function() {
+    // var volume = 0;
+    // volume = analyzer.getLevel();
+    // volume = map(volume, 0, 1, 0, height);
+    // h = volume;
+    line(this.x1, this.y1, this.x2, this.y2);
   }
 }
